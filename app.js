ff9f6d881607a2eb184afaf5ad6549cdb582b1f3
Sofie <solianyksofie@gmail.com>
2024-09-17 20:13:22 +0200

    another solution

--- app.js
+++ app.js
@@ -3,15 +3,43 @@
 // поверне нам "heo wor". 
 // Вихідний рядок та символи для видалення задає користувач.
 
+// у функції створити пусту змінну
+// циклом пройти по всім елементам рядка
+// Якщо поточний символ відсутній в масиві символів lettersToRemove - додаємо його до змінної яку створили раніше через 
+// resultStr += text[i]
+
 function removeLetters(text, lettersToRemove){
-   let resultStr = text;
-   for (let i = 0; i < lettersToRemove.length; i++){
-    let letter = lettersToRemove[i];
-    resultStr = resultStr.split(letter).join('');
+   let resultStr = '';
+
+   for(let i = 0; i<text.length; i++){
+       if(!lettersToRemove.includes(text[i])){
+           resultStr+=text[i];
+       }
    }
    return resultStr;
 }
 
-result = removeLetters("hello world", ['l', 'd']);
-console.log(result);
+userText = prompt(`Hi! Enter your text, please:`);
+userLettersToRemove = prompt(`Enter letters you'd like to remove from your text using a space between symbols: `);
+userLettersToRemove = userLettersToRemove.split(' ');
+
+let result = removeLetters(userText, userLettersToRemove);
+console.log(`Text you entered: ${userText}`);
+console.log(`Letters you want to remove: ${userLettersToRemove}`);
+console.log(`New text: ${result}`);
+
+
+
+// ----------------------------------------------------
+// function removeLetters(text, lettersToRemove){
+//    let resultStr = text;
+//    for (let i = 0; i < lettersToRemove.length; i++){
+//     let letter = lettersToRemove[i];
+//     resultStr = resultStr.split(letter).join('');
+//    }
+//    return resultStr;
+// }
+
+// result = removeLetters("hello world", ['l', 'd']);
+// console.log(result);
 
